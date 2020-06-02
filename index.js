1565c384e6b29fa4d90c65bf89d291502115c5e6
cpcwood <cpcwood@outlook.com>
2020-06-02 22:38:49 +0100

    remove deasync since it is not reliable on macos, use spawnSync instead to force process synchronicity

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 "use strict"
 
 var childProcess = require('child_process')
-var deasync = require('deasync')
 var path = require('path')
 var rubyTransformerPath = path.join(__dirname, 'erb_transformer.rb')
 
 function erbTransformer(fileContent) {
-  var child = childProcess.spawn('ruby', [rubyTransformerPath], { stdio: ['pipe', 'pipe', process.stderr] })
-  var childProcessClosed = false
-  var dataBuffers = []
-
-  child.stdout.on('data', (data) => {
-    dataBuffers.push(data)
-  })
-
-  child.on('close', function(code, signal) {
-    childProcessClosed = true
-  })
-
-  child.stdin.write(fileContent)
-  child.stdin.end()
- 
-  // Currently Jest transformers must be synchronous and cannot return promises
-  deasync.loopWhile( () => !childProcessClosed )
-  
-  var compiledFile = dataBuffers.map( b => b.toString() ).join('')
+  var child = childProcess.spawnSync(
+    'ruby', 
+    [ 
+      rubyTransformerPath
+    ], 
+    { 
+      stdio: ['pipe', 'pipe', process.stderr],
+      input: fileContent
+    }
+  )
+  var compiledFile = child.stdout.toString()
   return compiledFile
 }
 
